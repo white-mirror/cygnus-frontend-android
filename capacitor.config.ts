8d4b853af9bcfa2ec5e3b4bd92f505bb63d90dcc
Nicolas Panaino <npanaino@gmail.com>
2025-10-24 21:06:34 +0000

    chore: update web build path

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "com.cygnus.app",
   appName: "Cygnus",
-  webDir: "../frontend-web/dist",
+  webDir: "../web/dist",
   bundledWebRuntime: false,
   server: {
     url: process.env.CAP_SERVER_URL ?? process.env.VITE_API_BASE_URL,
